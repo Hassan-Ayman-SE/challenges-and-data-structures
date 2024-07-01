e1e9d76ed85f7bc34e6973209b7b573b2acce103
Hassan Karraz <24042732@student.ltuc.com>
2024-07-01 09:53:35 +0300

    SOLVE Challenge 1

--- challenges-and-data-structures/Program.cs
+++ challenges-and-data-structures/Program.cs
@@ -5,6 +5,74 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            //int[] array = { 1, 2, 3, 4, 5 };
+            //int[] reversedArray = ArrayReversal(array);
+
+            //Console.WriteLine("Array Before Reverse: " + string.Join(", ", array));
+            //Console.WriteLine("Array After Reverse: " + string.Join(", ", reversedArray));
+
+            // Challenge B: Most Frequent Number
+            int[] array = { 1, 1, 2, 2, 3, 4, 5, 5, 5, 6 };
+            int mostFrequentNumber = FindMostFrequentNumber(array);
+            Console.WriteLine("Most frequent number: " + mostFrequentNumber);
+            Console.ReadKey();
         }
+
+
+        //Challange A: Array Reversal
+        public static int[] ArrayReversal(int[] array)
+        {
+            //get the length of the array
+            int arrLength = array.Length;
+            //create a reversed array with the same length of original array.
+            int[] reversedArray = new int[arrLength];
+
+            //for loop to store numbers in reversedArray
+            for (int i = 0; i < arrLength; i++)
+            {
+                reversedArray[i] = array[arrLength - i - 1];
+            }
+
+            return reversedArray;
+        }
+
+
+        // Challenge B: Most Frequent Number
+        public static int FindMostFrequentNumber(int[] array)
+        {
+            // Use dictionary to store number counts
+            Dictionary<int, int> numberCounts = new Dictionary<int, int>();
+            // Count each number how many times it occurs in the array 
+            foreach (int number in array)
+            {
+                if (numberCounts.ContainsKey(number))
+                {
+                    // If number already exists in the dictionary increment its count
+                    numberCounts[number]++;
+                }
+                else
+                {
+                    // If number does not exist in the dictionary add it with initial count 1
+                    numberCounts[number] = 1;
+                }
+            }
+            int mostFrequentNumber = array[0];
+            int maxCount = 1;
+            // Find number with the maximum count
+            foreach (var item in numberCounts)
+            {
+                if (item.Value > maxCount)
+                {
+                    // If current count is higher than the previous maxCount
+                    // update mostFrequentNumber and maxCount
+                    mostFrequentNumber = item.Key;
+                    maxCount = item.Value;
+                }
+            }
+            // Return the most frequent number
+            return mostFrequentNumber;
+        }
+
+
     }
 }
