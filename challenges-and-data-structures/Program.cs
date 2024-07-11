23dcff1b0824995b64d607b12d5c8df5d85aa263
Hassan Karraz <24042732@student.ltuc.com>
2024-07-11 11:51:28 +0300

    Linked List Implementation

--- challenges-and-data-structures/Program.cs
+++ challenges-and-data-structures/Program.cs
@@ -1,35 +1,38 @@
-﻿namespace challenges_and_data_structures
+﻿using challenges_and_data_structures.Data_Structures.LinkedList;
+
+namespace challenges_and_data_structures
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            var example1 = "csharp is programming language";
-            var example2 = "Reverse the words in this sentence";
-            var example3 = "challenges and data structures";
+            LinkedList list = new LinkedList();
 
-            Console.WriteLine($"Input: \"{example1}\"");
-            Console.WriteLine($"Output: \"{ReverseWords(example1)}\"");
-            Console.WriteLine();
-            Console.WriteLine($"Input: \"{example2}\"");
-            Console.WriteLine($"Output: \"{ReverseWords(example2)}\"");
-            Console.WriteLine();
-            Console.WriteLine($"Input: \"{example3}\"");
-            Console.WriteLine($"Output: \"{ReverseWords(example3)}\"");
-            Console.WriteLine();
+            // Add nodes to the list
+            list.Add(5);
+            list.Add(10);
+            list.Add(20);
+            list.Add(30);
+
+            // Print the list
+            Console.WriteLine("Original list:");
+            list.PrintList(); // Output: Head -> 5 -> 10 -> 20 -> 30 -> Null
+
+            // Remove a node from the list
+            Console.WriteLine("Removing node with value 10:");
+            list.Remove(10);
+            list.PrintList(); // Output: Head -> 5 -> 20 -> 30 -> Null
+
+            // Check if the list includes a certain value
+            Console.WriteLine("Checking if the list includes value 20:");
+            bool includes20 = list.Includes(20);
+            Console.WriteLine(includes20); // Output: True
+
+            Console.WriteLine("Checking if the list includes value 10:");
+            bool includes10 = list.Includes(10);
+            Console.WriteLine(includes10); // Output: False
             Console.ReadKey();
         }
-        public static string ReverseWords(string input)
-        {
-            if (string.IsNullOrWhiteSpace(input))
-            {
-                return input;
-            }
-
-            var words = input.Split(' ');
-            Array.Reverse(words);
-            return string.Join(" ", words);
-        }
-
+   
     }
 }
