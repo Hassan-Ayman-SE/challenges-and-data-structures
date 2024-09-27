c1be42c7548c7c7f9493e9e527ad1e3b36e657b9
Hassan Karraz <24042732@student.ltuc.com>
2024-09-28 00:38:15 +0300

    Solve CC Binary Tree - Print Right View

---
+++ ChallengesTests/RightViewTests.cs
@@ -0,0 +1,47 @@
+﻿using challenges_and_data_structures.Data_Structures.Trees.TreeImplementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengesTests
+{
+    public class RightViewTests
+    {
+        [Fact]
+        public void RightView_MultipleNodes_ShouldReturnCorrectOutput()
+        {
+            BinaryTree tree = new BinaryTree();
+            tree.Root = new TNode(2);
+            tree.Root.Left = new TNode(3);
+            tree.Root.Right = new TNode(5);
+            tree.Root.Left.Left = new TNode(4);
+            tree.Root.Right.Right = new TNode(6);
+            tree.Root.Left.Left.Right = new TNode(7);
+
+            // Capture console output
+            var output = new StringWriter();
+            Console.SetOut(output);
+            tree.PrintRightView();
+
+            Assert.Equal("2 5 6 7 ", output.ToString());
+        }
+
+        [Fact]
+        public void RightView_RightOnlyNodes_ShouldReturnCorrectOutput()
+        {
+            BinaryTree tree = new BinaryTree();
+            tree.Root = new TNode(1);
+            tree.Root.Right = new TNode(2);
+            tree.Root.Right.Right = new TNode(3);
+
+            var output = new StringWriter();
+            Console.SetOut(output);
+            tree.PrintRightView();
+
+            Assert.Equal("1 2 3 ", output.ToString());
+        }
+    }
+
+}
---
+++ challenges-and-data-structures/Data-Structures/Trees/RightViewPrint/README.md
@@ -0,0 +1,9 @@
+# Binary Tree - Print Right View
+
+### Print the right view of a binary tree, showing the nodes visible from the right side at each level.
+
+## Whiteboard:
+![Binary Tree](bt-print-right-view-whiteboard.png)
+
+## Code run:
+![Code run](bt-print-right-view-run.PNG)
